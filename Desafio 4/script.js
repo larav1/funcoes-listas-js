dc790a81b82f051ffe101b59d247f1420badabfa
larav1 <l.vitorinha09@gmail.com>
2025-02-26 15:09:13 -0300

    :rocket: Desafio 4 resolução

--- Desafio 4/script.js
+++ Desafio 4/script.js
@@ -1,1 +1,21 @@
 // 1
+let listaGenerica = [];
+
+// 2
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+// 3
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+console.log(linguagensDeProgramacao);
+
+// 4
+let seriesDeTerror = ["From", "American Horror Story", "Slasher"];
+console.log(seriesDeTerror[0]);
+
+// 5
+let amigas = ["Vanessa", "Thayanne", "Lorena"];
+console.log(amigas[1]);
+
+// 6
+let amigos = ["Pablo", "Gabriel", "George"];
+console.log(amigos[2]);
